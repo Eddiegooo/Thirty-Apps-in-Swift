3417df6e22d32eefee1a52442941445a20eb01c3
Eddiegooo <nmgfqlong@126.com>
2019-03-17 22:10:25 +0800

    15

--- day16-3DTouch/day16-3DTouch/ViewController.swift
+++ day16-3DTouch/day16-3DTouch/ViewController.swift
@@ -7,8 +7,18 @@
 //
 
 import UIKit
+import SafariServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UIViewControllerPreviewingDelegate {
+    func previewingContext(_ previewingContext: UIViewControllerPreviewing, viewControllerForLocation location: CGPoint) -> UIViewController? {
+        previewingContext.sourceRect = CGRect(x: 40, y: 50, width: 300, height: 500)
+        return SFSafariViewController.init(url: (URL.init(string: "https:www.baidu.com") ?? nil)!)
+    }
+    
+    func previewingContext(_ previewingContext: UIViewControllerPreviewing, commit viewControllerToCommit: UIViewController) {
+        self.show(viewControllerToCommit, sender: self)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
