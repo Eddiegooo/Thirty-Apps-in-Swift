654c530c0d72bbec5d1b40de333a397a0b0212d0
范清龙 <fanqinglong@globalegrow.com>
2019-03-22 18:31:25 +0800

    day18 Today Widght

--- day18-Today-widght/Widght/TodayViewController.swift
+++ day18-Today-widght/Widght/TodayViewController.swift
@@ -9,13 +9,54 @@
 import UIKit
 import NotificationCenter
 
+/// 注意： Today-Widght  视图不可以滚动，不可以左右y左右滑动。  可以展示更多、更少
 class TodayViewController: UIViewController, NCWidgetProviding {
         
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view from its nib.
-    }
+        // 显示展示更多按键
+        self.extensionContext?.widgetLargestAvailableDisplayMode = .expanded
         
+        self.view.backgroundColor = UIColor.cyan
+        self.view.addSubview(customBtn) //自定义视图的frame 值得研究下。。
+        self.view.addSubview(detailLabel)
+        
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        detailLabel.text = UserDefaults.standard.object(forKey: "Widght") as? String
+    }
+    
+    
+    @objc func transferToApp() {
+        let url = URL(string: "Widght://")
+        self.extensionContext?.open(url!, completionHandler: nil)
+    }
+    
+    lazy var customBtn: UIButton = {
+        let btn = UIButton(type: .custom)
+        btn.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: 100)
+        btn.setTitle("来了老弟，点我啊", for: .normal)
+        btn.setTitleColor(UIColor.red, for: .normal)
+        btn.setImage(UIImage(named: ""), for: .normal)
+        btn.backgroundColor = UIColor.white
+        btn.addTarget(self, action: #selector(transferToApp), for: UIControl.Event.touchUpInside)
+        return btn
+    }()
+    
+    lazy var detailLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 20, y: 200, width: self.view.frame.width - 50, height: 100))
+        label.text = "test"
+        label.textColor = UIColor.black
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
         // Perform any setup necessary in order to update the view.
         
@@ -25,5 +66,21 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         
         completionHandler(NCUpdateResult.newData)
     }
+    
+    ///展示更多、收起c折叠
+    func widgetActiveDisplayModeDidChange(_ activeDisplayMode: NCWidgetDisplayMode, withMaximumSize maxSize: CGSize) {
+        ///这个高度最大也不会高于屏幕高。  随便设置你想的高度
+        if activeDisplayMode == .expanded {
+            self.preferredContentSize = CGSize(width: maxSize.width, height: 400)
+        }else {
+            self.preferredContentSize = maxSize
+        }
+    }
+    
+    
+    ///这个设置边距方法已经废弃了。。。
+//    func widgetMarginInsets(forProposedMarginInsets defaultMarginInsets: UIEdgeInsets) -> UIEdgeInsets {
+//        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+//    }
     
 }
--- day18-Today-widght/day18-Today-widght/ViewController.swift
+++ day18-Today-widght/day18-Today-widght/ViewController.swift
@@ -10,13 +10,39 @@ import UIKit
 
 /// 1.新建：
 /// File --》 New --》Target --》Today Extention。  新建运行， 就会在TodayWidght 页面显示本APP了
+/// 2.设置Today界面内容
+/// 3.实现跳转。 URLType
+/// 4.内容是通过NUserDefault传递的。
+/// 注意： 怎么让Widght 和 App对应起来呢？
+//1. App 上打开 App group 。添加自己的BundleID
+//2. 在Widght 上进行同样的操作，打开 App group， 勾选对应的BundleID
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.red
+        
+        self.view.addSubview(detailLabel)
+        
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UserDefaults.standard.set("我是Widght， 我要传值进来啦", forKey: "Widght")
+        UserDefaults.standard.synchronize()
+    }
+    
+    
 
-
+    lazy var detailLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 20, y: 200, width: self.view.frame.width - 50, height: 100))
+        label.text = "test"
+        label.textColor = UIColor.black
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
 }
 
