b348e37204a364a76fa5dbb166eb0a0f2fb7d865
范清龙 <fanqinglong@globalegrow.com>
2019-03-30 11:29:37 +0800

    day21 BaseAnimation

--- day21-BaseAnimation/day21-BaseAnimation/AnimationViewController.swift
+++ day21-BaseAnimation/day21-BaseAnimation/AnimationViewController.swift
@@ -15,19 +15,55 @@ class AnimationViewController: UIViewController {
 
         self.view.backgroundColor = .white
         self.view.addSubview(customImage)
+        springAnimation()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         baseAnimation()
     }
     
     func baseAnimation () -> Void {
-//        Spring: 模拟弹簧弹跳的效果
-//        参数: Damping:阻尼 0-1 阻尼越小动画越明显
-//        参数: initialSpringVelocity : 动画初始变化速度
+        /// MARK: 分步骤动画
+        UIView.beginAnimations("Animation", context: nil) //开始动画
+        UIView.setAnimationDelegate(self)
+        UIView.setAnimationRepeatAutoreverses(true)
+        UIView.setAnimationDuration(2)
+        UIView.setAnimationCurve(UIView.AnimationCurve.linear)
+        UIView.setAnimationRepeatCount(10)
+        UIView.setAnimationWillStart(#selector(animationStart))
+        UIView.setAnimationDidStop(#selector(animationEnd))
+        // 具体动画
+        UIView.setAnimationTransition(UIView.AnimationTransition.flipFromLeft, for: self.customImage, cache: true)
+        UIView.commitAnimations()//结束动画
+        
+        
+//        iOS 4之后都使用Block 块了..
+        UIView.animate(withDuration: 2, animations: {
+            print("Animation")
+        }) { (finish) in
+            print("Animation End")
+        }
+    }
+    
+    @objc func animationStart() {
+        print("Animation start")
+    }
+    
+    @objc func animationEnd() {
+        print("Animatoin End")
+    }
+    
+    func springAnimation () -> Void {
+        //        Spring: 模拟弹簧弹跳的效果
+        //        参数: Damping:阻尼 0-1 阻尼越小动画越明显
+        //        参数: initialSpringVelocity : 动画初始变化速度
         UIView.animate(withDuration: 2, delay: 0.5, usingSpringWithDamping: 0.1, initialSpringVelocity: 10, options: UIView.AnimationOptions.curveEaseInOut, animations: {
             self.customImage.frame.origin.x = 200
         }) { (isFinish) in
-//            self.customImage.frame =  CGRect(x: 50, y: 100, width: 200, height: 200)
+            //            self.customImage.frame =  CGRect(x: 50, y: 100, width: 200, height: 200)
         }
     }
+    
     
     lazy var customImage: UIImageView = {
         let imageV = UIImageView(frame: CGRect(x: 50, y: 100, width: 200, height: 200))
@@ -57,8 +93,8 @@ class LineViewController: UIViewController {
         let keyFrame = CAKeyframeAnimation(keyPath: "position")
     
         //  建几个动画的位置
-        let p1 = CGPoint(x: 20, y: 100);
-        let p2 = CGPoint(x:20, y: 400);
+        let p1 = CGPoint(x:20,  y: 100);
+        let p2 = CGPoint(x:20,  y: 400);
         let p3 = CGPoint(x:400, y: 100);
         let p4 = CGPoint(x:100, y: 200);
         let p5 = CGPoint(x:200, y: 300);
@@ -73,13 +109,34 @@ class LineViewController: UIViewController {
         // 每一帧的时间,  每一帧的时间为比例的累加计算  取值范围 0-1,没有设置的话 每一帧的时间是平分的
         keyFrame.keyTimes = [0.1,0.2,0.3,0.4];
         self.customImage.layer.add(keyFrame, forKey: "随便你写啥，系统保留关键字")
+        
+        //Block 块形式  感觉这个还没有上面简单 好用
+        UIView.animateKeyframes(withDuration: 4, delay: 0, options: UIView.KeyframeAnimationOptions.autoreverse, animations: {
+            // 关键帧动作。。
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 1, animations: {
+                
+            })
+            
+            // 关键帧动作。。
+            UIView.addKeyframe(withRelativeStartTime: 1, relativeDuration: 1, animations: {
+                
+            })
+            
+        }) { (finish) in
+            
+        }
+        
     }
     
     lazy var customImage: UIImageView = {
         let imageV = UIImageView(frame: CGRect(x: 50, y: 100, width: 200, height: 200))
         imageV.center = self.view.center
         imageV.contentMode = .scaleAspectFit
-        imageV.image = UIImage(named: "666")
+        /// 直接设置图片方法： image Literal --> 出来占位图，双击， 然后选择你需要的图片。。
+        // 或者：cmd + shift + m，也可调出来所有的图片资源， 可以拖动到工程里
+        imageV.image = #imageLiteral(resourceName: "666")
+        imageV.image = #imageLiteral(resourceName: "666.jpg")
+//        imageV.image = UIImage(named: "666")
         return imageV
     }()
 }
@@ -172,14 +229,23 @@ class AlphaViewController: UIViewController {
         self.view.addSubview(customImage)
         // Do any additional setup after loading the view.
         self.customImage.alpha = 0
-        
         pageModeAnimation()
     }
     
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        UIView.animate(withDuration: 1) {
+        
+        /// ios 9, swift 也可以使用这个新特性做渐变动画
+        let effectView = UIVisualEffectView(frame: self.customImage.frame)
+        self.customImage.addSubview(effectView)
+        let effect = UIBlurEffect(style: .dark)
+        effectView.effect = nil
+        UIView.animate(withDuration: 0.5) {
+            effectView.effect = effect
+        }
+        
+        UIView.animate(withDuration: 2) {
             self.customImage.alpha = 1
         }
     }
