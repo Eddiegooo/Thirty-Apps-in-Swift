2cd7bb2917faaf31588e9f26a04ae402dcd316ef
范清龙 <fanqinglong@globalegrow.com>
2019-06-15 15:06:39 +0800

    day 28

--- day28-WikiFace/day28-WikiFace/ViewController.swift
+++ day28-WikiFace/day28-WikiFace/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    var wikiImageView: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.addSubview(self.textField)
+        let imageView = UIImageView(frame: CGRect(x: 30, y: 150, width: self.view.frame.size.width - 60, height: self.view.frame.size.height - 200))
+        imageView.backgroundColor = UIColor.white
+        self.wikiImageView = imageView
+        self.view.addSubview(imageView)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    /// MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        return true
     }
-
+    
+    lazy var textField: UITextField = {
+        let text = UITextField(frame: CGRect(x: 20, y: 80, width: self.view.frame.size.width - 50, height: 40))
+        text.textColor = UIColor.blue
+        text.borderStyle = UITextBorderStyle.roundedRect
+        text.placeholder = "Please enter you want to search"
+        text.delegate = self as UITextFieldDelegate
+        return text
+    }()
+    
+    
 
 }
 
---
+++ day28-WikiFace/day28-WikiFace/WikiFaceObject.swift
@@ -0,0 +1,20 @@
+//
+//  WikiFaceObject.swift
+//  day28-WikiFace
+//
+//  Created by FQL on 2019/6/15.
+//  Copyright © 2019 FQL. All rights reserved.
+//
+
+import UIKit
+
+class WikiFaceObject: NSObject {
+    // 枚举
+    enum ImageRequest: Error {
+        case ImageRequestLoadError
+    }
+    
+    class func faceToPerson(_ person: String, size: CGSize) -> Void {
+        
+    }
+}
